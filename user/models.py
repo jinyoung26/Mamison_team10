2c50b2d6c2fa4ce70818a86eb9e7ca97b87e493e
KooHyunJung <ghj3160@gmail.com>
2022-02-04 09:02:11 +0900

    UserApp setting.py 코드 기입

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "my_user"
+
+    # like one-to-many 임으로 따로 생성
+    bio = models.TextField(max_length=500, blank=True)
