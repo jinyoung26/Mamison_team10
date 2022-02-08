83cf39d3196bea9d87f1bbce352b8f6c11191fed
김현두 <bbb6045@naver.com>
2022-02-08 14:18:42 +0900

    최신 데이터 모델 적용
    Merge branch 'main' of https://github.com/jinyoung26/mamison_team10 into ddd
    
    # Conflicts:
    #	.idea/dataSources.xml
    #	db.sqlite3
    #	templates/user/signin.html
    #	tweet/urls.py
    #	tweet/views.py
    #	user/models.py

--- tweet/db.py
+++ tweet/db.py
@@ -7,14 +7,14 @@ import django
 django.setup()
 from tweet.models import tweetmodel
 from user.models import  UserModel
-with open('../static/model/rcp_data_v5.csv','rt',encoding='UTF8') as f:
+with open('../static/model/rcp_data_8888_v66.csv','rt',encoding='UTF8') as f:
     dr = csv.DictReader(f)
     s = pd.DataFrame(dr)
 ss = []
 user=UserModel.objects.get(username="bbb6045")
 
 for i in range(len(s)):
-    st = (s['id'][i], s['url'][i], s['tag'][i], s['title'][i], s['des'][i], s['ing'][i], s['img_src'][i])
+    st = (s['id'][i], s['url'][i], s['tag'][i],s['food'][i], s['title'][i], s['des'][i], s['ing'][i], s['img_src'][i])
     ss.append(st)
 for i in range(len(s)):
-    tweetmodel.objects.create( author=user ,r_id=ss[i][0], url=ss[i][1], tag=ss[i][2], title=ss[i][3], des=ss[i][4], ing=ss[i][5], img_src=ss[i][6])
+    tweetmodel.objects.create( author=user ,r_id=ss[i][0], url=ss[i][1], tag=ss[i][2],food=ss[i][3], title=ss[i][4], des=ss[i][5], ing=ss[i][6], img_src=ss[i][7])
--- tweet/models.py
+++ tweet/models.py
@@ -11,6 +11,7 @@ class tweetmodel(models.Model):
     r_id = models.CharField(max_length=9)
     url = models.CharField(max_length=45)
     tag = models.CharField(max_length=43)
+    food= models.CharField(max_length=43)
     title = models.CharField(max_length=100)
     des = models.CharField(max_length=200)
     ing = models.CharField(max_length= 200)
